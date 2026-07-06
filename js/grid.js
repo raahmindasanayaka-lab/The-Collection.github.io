05c70d0ff4f37172e624af63a5debabd0163bbed
Rashmin <raahmindasanayaka@gmail.com>
2026-07-06 09:39:46 +0530

    Upgrade grid.js with ES6 modern syntax

--- js/grid.js
+++ js/grid.js
@@ -1,4 +1,4 @@
-let products = [
+const products = [
   { name: "Cotton Hoodie",    price: 2000, color: "Black",  featured: true  },
   { name: "Brown Hoodie",     price: 2000, color: "Brown",  featured: false },
   { name: "Black Hoodie",     price: 2500, color: "Black",  featured: false },
@@ -6,19 +6,20 @@ let products = [
   { name: "XL Hoodie",        price: 3500, color: "Navy",   featured: false }
 ];
 
-let grid = document.getElementById("product-grid");
+const grid = document.getElementById("product-grid");
 
-products.forEach(product => {
-  let card = document.createElement("div");
-  card.className = product.featured ? "product-card featured" : "product-card";
+const cardHTML = product.map(({ name, price, color, featured }) => {
 
-  card.innerHTML = `
-        <img src="images/cotton.png" alt="${product.name}">
-        <h2>${product.name}</h2>
-        <p>${product.color}</p>
-        <p><b>Rs.${product.price}</b></p>
-        <button>Order Now</button>
-    `;
-
-  grid.appendChild(card);
+  const cardClass = featured ? "product-card featured" : "product-card";
+  return `
+  <div class="${cardClass}">
+  <img src="images/cotton.png" alt="${name}">
+  <h2>${name}</h2>
+  <p>${color}</p>
+  <p><b>Rs.${price}</b></p>
+  <button>Order Now</button>
+  </div>
+  `;
 });
+
+grid.innerHTML = cardHTML.join("");
